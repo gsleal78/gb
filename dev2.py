d168c7f11fa588d1b0af08bed661caaf04439376
Bruno Fava <bruno.fava98@outlook.com>
2024-04-25 14:25:29 -0300

    mudança1

--- dev2.py
+++ dev2.py
@@ -1,1 +1,46 @@
-print("Guilherme Leal") 
+from random import randint
+def cria_mapa(n):
+    l = []
+    l2 = []
+    for x in range(n):
+        l2.append(' ')
+    for x in range(n):
+        l.append(l2)
+    return l
+def posicao_suporta(m,n,l,c,o):
+    if o=='h':
+        if c+n>len(m[0]):
+            return False
+        for i in range(n):
+            if m[l][i+c]=='N':
+                return False
+        return True
+    elif o=='v':
+        if l+n>len(m):
+            return False
+        for i in range(n):
+            if m[l+i][c]=='N':
+                return False
+        return True
+def aloca_navios(m, l):
+    for i in l:
+        while True:
+            linha = random.randint(0, len(m) - 1)
+            coluna = random.randint(0, len(m[0]) - 1)
+            orientacao = random.choice(['h', 'v'])
+            if posicao_suporta(m, i, linha, coluna, orientacao):
+                break
+        if orientacao == 'h':
+            for a in range(i):
+                m[linha][a + coluna] = 'N'
+        else:
+            for a in range(i):
+                m[linha + a][coluna] = 'N'
+    return m
+def foi_derrotado(l):
+    for i in range(len(l)):
+        for k in range(len(l[i])):
+            if l[i][k]=='N':return False
+    return True
+mapa = cria_mapa(10)
+print(mapa)
