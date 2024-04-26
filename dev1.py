6f828a18f24c63aade0c47b809b22ecf81de5961
Guilherme <guilhermesl7@al.insper.edu.br>
2024-04-26 14:57:22 -0300

    aloca navios para o usuario

--- dev1.py
+++ dev1.py
@@ -1,3 +1,20 @@
+def posicao_suporta(m,n,l,c,o):
+    if o=='h':
+        if c+n>len(m[0]):
+            return False
+        for i in range(n):
+            if m[l][i+c]=='N':
+                return False
+        return True
+    elif o=='v':
+        if l+n>len(m):
+            return False
+        for i in range(n):
+            if m[l+i][c]=='N':
+                return False
+        return True
+
+
 DICIONARIO_CORES = {
     "A": '\u001b[34m▓▓▓\u001b[0m',
     "X": '\u001b[31m▓▓▓\u001b[0m'
@@ -13,14 +30,29 @@ def verifica_stb(posicao):
     
 def converte_coordenada(coordenada):
     letra = coordenada[0].lower()
-    coluna = coordenada[1]
+    coluna = int(coordenada[1])-1
     for k,v in d.items(): 
         if letra == k: 
             linha = v
+    coordenada2 = [linha,coluna]
+    return coordenada2
 
 def valida_coordenada(coordenada):
     if coordenada[0] in 'ABCDEFGHIJ' and int(coordenada[1]) in range(1,11): 
         return True 
     return False 
+
+def aloca_navios_jo(m, l,linha,coluna,orientacao):
+    for i in l:
+        if posicao_suporta(m, i, linha, coluna, orientacao):       
+            if orientacao == 'h':
+                for a in range(i):
+                    m[linha][a + coluna] = 'N'
+            else:
+                for a in range(i):
+                    m[linha + a][coluna] = 'N'
+        else: 
+            return False
+    return m
 
 
