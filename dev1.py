4816dc291735d9341ad2aa9c1066e0882c09ce64
Guilherme <guilhermesl7@al.insper.edu.br>
2024-04-26 11:17:12 -0300

    concertando dev1

--- dev1.py
+++ dev1.py
@@ -7,24 +7,6 @@ def cria_mapa(n):
         l.append(l2)
     return l
 
-print(cria_mapa(10))
-
 def printa_string(l):
-    string=''
-    for i in l:
-        for k in i:
-            string +=k
-        string+='\n'
-    return string
-i = 0 
-for s in mi: 
-    if i == 0: 
-        print(s)
-        i+=1 
-    else: 
-        time.sleep(2)        
-        print(s)
-escolhe_pais = input("Qual o número da nação da sua frota?")
-print("Otima escolha")
-time.sleep(1)
-print("Iniciando jogo!")
+    print("COMPUTADOR - Austrália                   JOGADOR - Austrália")
+    print("    A  B  C  D  E  F  G  H  I  J          A  B  C  D  E  F  G  H  I  J ")
